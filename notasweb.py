4356fce0aeb097f1a89676374d7933d9ecf393f0
Dato Simó <dato@net.com.org.es>
2016-03-17 15:22:59 -0300

    Usar itsdangerous para firmar.

--- notasweb.py
+++ notasweb.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 import os
-import hashlib
 
 import flask
 import flask_wtf
+import itsdangerous
 
-from webargs import Arg, ValidationError
-from webargs.flaskparser import FlaskParser
+from webargs import fields as wfields
+from webargs.flaskparser import use_args
 
 from wtforms import fields
 from wtforms import validators
@@ -21,12 +21,7 @@ app.secret_key = os.environ["NOTAS_SECRET"]
 app.config.title = "Notas de " + os.environ["NOTAS_COURSE_NAME"]
 
 assert app.secret_key
-
-
-@app.errorhandler(400)
-def bad_request(err):
-    return flask.render_template(
-        "error.html", message=str(err.data["message"])), 400
+signer = itsdangerous.URLSafeSerializer(app.secret_key)
 
 
 class Formulario(flask_wtf.Form):
@@ -34,17 +29,19 @@ class Formulario(flask_wtf.Form):
     """
     padron = fields.StringField(
         "Padrón", validators=[
-            validators.Regexp(r"\w+", message="Ingresár un padrón válido")])
+            validators.Regexp(r"\w+", message="Ingrese un padrón válido")])
 
     email = html5.EmailField(
         "E-mail", validators=[
-            validators.Email(message="Ingresar una dirección de mail válida")])
+            validators.Email(message="Ingrese una dirección de e-mail válida")])
 
     submit = fields.SubmitField("Obtener enlace")
 
 
 @app.route("/", methods=('GET', 'POST'))
 def index():
+    """Sirve la página de solicitud del enlace.
+    """
     form = Formulario()
 
     if form.validate_on_submit():
@@ -65,23 +62,27 @@ def index():
     return flask.render_template("index.html", form=form)
 
 
-@app.route("/consultar")
-def consultar():
-    args = {
-        "key": Arg(str, required=True),
-        "padron": Arg(str, required=True),
-    }
-    def validar(args):
-        if args["key"] == genkey(args["padron"]):
-            return True
-        else:
-            raise ValidationError("Parámetro ‘padron’ o ‘key’ no válido")
+@app.errorhandler(422)
+def bad_request(err):
+    """Se invoca cuando falla la validación de la clave.
+    """
+    return flask.render_template( "error.html", message="Clave no válida")
 
-    result = FlaskParser().parse(args, validate=validar)
 
+def validate(value):
+    # Needed because URLSafeSerializer does not have a validate().
     try:
-        notas_alumno = notas.notas(result["padron"])
-    except (IndexError, KeyError) as e:
+        return bool(signer.loads(value))
+    except itsdangerous.BadSignature:
+        return False
+
+
+@app.route("/consultar")
+@use_args({"clave": wfields.Str(required=True, validate=validate)})
+def consultar(args):
+    try:
+        notas_alumno = notas.notas(signer.loads(args["clave"]))
+    except IndexError as e:
         return flask.render_template("error.html", message=str(e))
     else:
         return flask.render_template("result.html", items=notas_alumno)
@@ -93,18 +94,11 @@ def norm_field(f):
     return f.data.strip().lower()
 
 
-def genkey(padron):
-    """Devuelve la clave asociada con un padrón.
-    """
-    secret = (padron + app.secret_key).encode("utf-8")
-    return hashlib.sha1(secret).hexdigest()
-
-
 def genlink(padron):
-    """Devuelve la dirección de consulta de un padrón.
+    """Devuelve el enlace de consulta para un padrón.
     """
-    return flask.url_for("consultar", padron=padron, key=genkey(padron),
-                         _external=True)
+    signed_padron = signer.dumps(padron)
+    return flask.url_for("consultar", clave=signed_padron, _external=True)
 
 
 if __name__ == "__main__":
