7f516c9b76ae61015fe14aaf24aeeee76e439759
tutecano1995 <tutecano1995@gmail.com>
2021-08-13 16:21:10 -0300

    Use service account

--- notas_oauth.py
+++ notas_oauth.py
@@ -2,33 +2,22 @@
 
 """Módulo para autenticación con OAuth2."""
 
-import datetime
-import httplib2
-import os
 import sys
 
-import oauth2client.client
+import google.oauth2.service_account
+import google.auth.transport.requests
 
-_CLIENT_ID = os.environ["NOTAS_OAUTH_CLIENT"]
-_CLIENT_SECRET = os.environ["NOTAS_OAUTH_SECRET"]
-_OAUTH_REFRESH = os.environ["NOTAS_REFRESH_TOKEN"]
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
-_creds = oauth2client.client.OAuth2Credentials(
-    "", _CLIENT_ID, _CLIENT_SECRET, _OAUTH_REFRESH,
-    datetime.datetime(2015, 1, 1),
-    "https://accounts.google.com/o/oauth2/token", "notasweb/1.0")
-
+_creds = google.oauth2.service_account.Credentials.from_service_account_file("./service_account.json", scopes=SCOPES)
 
 def get_credenciales():
     """Devuelve nuestro objeto OAuth2Credentials, actualizado.
 
     Esta función llama a _refresh() si el token expira en menos de 5 minutos.
     """
-    now = datetime.datetime.utcnow()
-    valid_until = _creds.token_expiry - datetime.timedelta(minutes=5)
-
-    if valid_until < now:
-        print("Generando nuevo token de acceso.", file=sys.stderr)
-        _creds.refresh(httplib2.Http())
+    if not _creds.valid:
+        print("Refrescando token de acceso...", file=sys.stderr)
+        _creds.refresh(google.auth.transport.requests.Request())
 
     return _creds
